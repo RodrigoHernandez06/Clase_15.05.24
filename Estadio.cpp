1330609820771b246f26aba0c6e0dc9a4041b75e
RodrigoHernandez06 <160176017+RodrigoHernandez06@users.noreply.github.com>
2024-05-15 20:47:41 -0600

    boleteria

---
+++ Estadio.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int cant=0, ticket=0, r=0;
+
+    cout<<"Bienvenido a la boleteria electronica del estadio XYZ. Que entrada desea comprar: \n"
+        <<"MAXIMO 4 POR PERSONA\n"
+        <<"1 - Sol General\n"
+        <<"2 - Sol Preferente\n"
+        <<"3 - Sombra\n"
+        <<"4 - Tribuna\n"
+        <<"5 - Platea\n";
+    cin>>ticket;
+
+    switch(ticket){
+        case 1:
+        cout<<"Cuantas entradas desea comprar? \n";
+        cin>>cant;
+        if(cant<=4){
+        r=cant*3;
+        cout<<"El total es de: $"<<r<<".00";
+        break;
+        }
+        else if(cant>4){
+            cout<<"Lo sentimos, el maximo es de 4 entradas por persona.\n";
+        }
+
+        case 2:
+        cout<<"Cuantas entradas desea comprar? \n";
+        cin>>cant;
+        if(cant<=4){
+        r=cant*5;
+        cout<<"El total es de: $"<<r<<".00";
+        break;
+        }
+        else if(cant>4){
+            cout<<"Lo sentimos, el maximo es de 4 entradas por persona.\n";
+        }
+
+        case 3:
+        cout<<"Cuantas entradas desea comprar? \n";
+        cin>>cant;
+        if(cant<=4){
+        r=cant*8;
+        cout<<"El total es de: $"<<r<<".00";
+        break;
+        }
+        else if(cant>4){
+            cout<<"Lo sentimos, el maximo es de 4 entradas por persona.\n";
+        }
+
+        case 4:
+        cout<<"Cuantas entradas desea comprar? \n";
+        cin>>cant;
+        if(cant<=4){
+        r=cant*15;
+        cout<<"El total es de: $"<<r<<".00";
+        break;
+        }
+        else if(cant>4){
+            cout<<"Lo sentimos, el maximo es de 4 entradas por persona.\n";
+        }
+
+        case 5:
+        cout<<"Cuantas entradas desea comprar? \n";
+        cin>>cant;
+        if(cant<=4){
+        r=cant*20;
+        cout<<"El total es de: $"<<r<<".00";
+        break;
+        }
+        else if(cant>4){
+            cout<<"Lo sentimos, el maximo es de 4 entradas por persona.\n";
+        }
+    }
+    return 0;
+}
